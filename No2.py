f9bebb97ee658598e2218766424422e69564d3ff
Ahmad Arifudin <wellyainy69@gmail.com>
2024-12-19 16:44:40 +0700

    Add files via upload

---
+++ No2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 16:44:08 2024
+
+@author: HP
+"""
+
+r = float(input("Masukkan jari-jari :"))
+Luas_Lingkaran = 3.14*pow(r,2)
+print(Luas_Lingkaran)
